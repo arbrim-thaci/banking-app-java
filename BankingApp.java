f848813592a35e7cdd9b5565c58bd63d4f2386f5
Arbrim Thaci X1 <arbrim.thaci@makerminds.org>
2024-03-07 21:21:56 +0100

    Added a comment that I was here with my MakerMinds students in Bonus Session Version Control Systems 03/24

--- BankingApp.java
+++ BankingApp.java
@@ -1,6 +1,7 @@
 public class BankingApp {
 
 	// This is the main method
+	// I was here with my students on 07.03.2024
 	public static void main(String[] args) {
 		System.out.println("Hello Banking App");
 	}
